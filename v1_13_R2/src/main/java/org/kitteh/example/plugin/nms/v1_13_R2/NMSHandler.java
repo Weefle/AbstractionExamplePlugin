68b6b48d27808a36f86961207b3266caafd241d0
Weefle <mr.weefle@gmail.com>
2022-05-06 00:43:24 +0200

    First Commit

---
+++ v1_13_R2/src/main/java/org/kitteh/example/plugin/nms/v1_13_R2/NMSHandler.java
@@ -0,0 +1,19 @@
+package org.kitteh.example.plugin.nms.v1_13_R2;
+
+import net.minecraft.server.v1_13_R2.IChatBaseComponent;
+
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_13_R2.util.CraftChatMessage;
+import org.bukkit.entity.Player;
+import org.kitteh.example.plugin.api.NMS;
+
+public class NMSHandler implements NMS {
+
+
+    public void sendMessage(Player player, String message) {
+        for(IChatBaseComponent component : CraftChatMessage.fromString(message)) {
+            ((CraftPlayer)player).getHandle().sendMessage(component);
+        }
+    }
+
+}
